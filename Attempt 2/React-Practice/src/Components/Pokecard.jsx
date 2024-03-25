8d55933909b8d1b655a064549bd71894f4a995de
Yashaswini J <yashaswinij1999>
2024-03-25 20:13:41 +0530

    styled the component

--- Attempt 2/React-Practice/src/Components/Pokecard.jsx
+++ Attempt 2/React-Practice/src/Components/Pokecard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Pokemon.css";
 
 function Pokecard() {
   const random = Math.floor(Math.random() * 150) + 1;
@@ -6,10 +7,10 @@ function Pokecard() {
     `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${random}` +
     `.png`;
   return (
-    <>
+    <div className="main">
       <h1>Pokemon #{random}</h1>
       <img src={url} alt="pokemon"></img>
-    </>
+    </div>
   );
 }
 
