579215df717f55b80bc45c9dbaffc48540ea13e9
Yashaswini J <yashaswinij1999>
2024-03-25 20:12:01 +0530

    rendering the image using the random number to generate the url

--- Attempt 2/React-Practice/src/Components/Pokecard.jsx
+++ Attempt 2/React-Practice/src/Components/Pokecard.jsx
@@ -2,7 +2,15 @@ import React from "react";
 
 function Pokecard() {
   const random = Math.floor(Math.random() * 150) + 1;
-  return <h1>Pokemon #{random}</h1>;
+  const url =
+    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${random}` +
+    `.png`;
+  return (
+    <>
+      <h1>Pokemon #{random}</h1>
+      <img src={url} alt="pokemon"></img>
+    </>
+  );
 }
 
 export default Pokecard;
