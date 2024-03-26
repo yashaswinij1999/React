efa972ae1ad0481d1fd1ec41aa1dc2e020d68443
Yashaswini J <yashaswinij1999>
2024-03-26 19:55:03 +0530

    passed props to slots component

--- Attempt 2/Slot-Machine/src/App.jsx
+++ Attempt 2/Slot-Machine/src/App.jsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Slots from "./Components/Slots";
 
 function App() {
-  return <>
-  </>;
+  return (
+    <>
+      <Slots val1="🍒" val2="🍒" val3="🍒" />
+    </>
+  );
 }
 
 export default App;
--- Attempt 2/Slot-Machine/src/Components/Slots.jsx
+++ Attempt 2/Slot-Machine/src/Components/Slots.jsx
@@ -1,5 +1,11 @@
-export default function Slots(){
-
-
-
+export default function Slots({ val1, val2, val3 }) {
+  return (
+    <>
+      <h1>
+        {val1}
+        {val2}
+        {val3}
+      </h1>
+    </>
+  );
 }
