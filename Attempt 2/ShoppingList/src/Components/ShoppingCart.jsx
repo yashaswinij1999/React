c1b6a58a7e8e77ad18687dff9dc216c3964c59aa
Yashaswini J <yashaswinij1999>
2024-03-26 21:11:57 +0530

    rendered

--- Attempt 2/ShoppingList/src/Components/ShoppingCart.jsx
+++ Attempt 2/ShoppingList/src/Components/ShoppingCart.jsx
@@ -3,10 +3,17 @@ import React from "react";
 export default function ShoppingCart(props) {
   const { id, items, quantity, completed } = props;
 
+  const style = {
+    textDecorationLine: completed ? "line-through" : "null",
+    color: completed ? "green" : "red",
+  };
+
   return (
     <>
       <ul>
-        <li>{items}</li>
+        <li style={style} key={id}>
+          {items} - {quantity}
+        </li>
       </ul>
     </>
   );
