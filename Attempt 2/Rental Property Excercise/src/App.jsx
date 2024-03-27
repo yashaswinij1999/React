7a166473145ce1a6f0a802e35145e02ed40b6c3b
Yashaswini J <yashaswinij1999>
2024-03-27 16:41:28 +0530

    styled properties list with css properties

--- Attempt 2/Rental Property Excercise/src/App.jsx
+++ Attempt 2/Rental Property Excercise/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import PropertyList from "./Components/PropertyList";
 
 function App() {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <PropertyList />
+    </>
+  );
 }
 
 export default App;
---
+++ Attempt 2/Rental Property Excercise/src/Components/PropertyList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./PropertyList.css";
+
+function PropertyList() {
+  return <div className="list">PropertyList</div>;
+}
+
+export default PropertyList;
