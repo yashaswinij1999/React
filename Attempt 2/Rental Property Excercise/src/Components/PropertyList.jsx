cae0c5c82d7cb205586e0f0c00c46d900c311317
Yashaswini J <yashaswinij1999>
2024-03-27 16:46:23 +0530

    looping over the data and rendering it to Property Component

--- Attempt 2/Rental Property Excercise/src/Components/PropertyList.jsx
+++ Attempt 2/Rental Property Excercise/src/Components/PropertyList.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import "./PropertyList.css";
 
-function PropertyList() {
-  return <div className="list">PropertyList</div>;
+function PropertyList({ properties }) {
+  return (
+    <>
+      <div className="list">
+        {properties.map((el) => (
+          <Property
+            id={el.id}
+            name={el.name}
+            rating={el.rating}
+            price={el.price}
+          />
+        ))}
+      </div>
+      ;
+    </>
+  );
 }
 
 export default PropertyList;
