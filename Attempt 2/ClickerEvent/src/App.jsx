f98c144a0d9acde7a4490e0e01f6d85253dbbab1
Yashaswini J <yashaswinij1999>
2024-03-28 20:56:25 +0530

    inital commit

---
+++ Attempt 2/ClickerEvent/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import ButtonClicker from "./Components/ButtonClicker";
+
+function App() {
+  return (
+    <>
+      <ButtonClicker />
+    </>
+  );
+}
+
+export default App;
---
+++ Attempt 2/ClickerEvent/src/Components/ButtonClicker.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function ButtonClicker() {
+
+    function ClickMe(){
+        alert('Thanks ')
+    }
+
+  return (
+    <>
+      <button>Click Me</button>
+      <button>Don't Click</button>
+    </>
+  );
+}
+
+export default ButtonClicker;
