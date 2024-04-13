7b1ddf66a898fbf6f72fdf40e8c37590d26e4a00
Yashaswini J <yashaswinij1999>
2024-04-13 11:25:26 +0530

    completed storing data to local storage

--- Attempt 1/Local Storage/List/src/App.jsx
+++ Attempt 1/Local Storage/List/src/App.jsx
@@ -5,25 +5,6 @@ import "./App.css";
 
 import List from "./Components/List";
 
-const data = [
-  {
-    id: 1,
-    task: "buy milk",
-  },
-  {
-    id: 2,
-    task: "study",
-  },
-  {
-    id: 3,
-    task: "help others in daily chores",
-  },
-  {
-    id: 4,
-    task: "buy groceries",
-  },
-];
-
 function App() {
   return (
     <>
