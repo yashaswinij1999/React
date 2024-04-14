b789ceb08952a36831c229be06d731ca5088dfa7
Yashaswini J <yashaswinij1999>
2024-04-14 15:41:43 +0530

    added one more state

--- Attempt 3/Toggler/src/Components/Toggler.jsx
+++ Attempt 3/Toggler/src/Components/Toggler.jsx
@@ -2,6 +2,11 @@ import { useState } from "react";
 
 export default function Toggler() {
   const [isHappy, setEmoji] = useState(true);
+  const [gender, setGender] = useState(true);
+
+  function handle() {
+    setGender(!gender);
+  }
 
   function toggle() {
     setEmoji(!isHappy);
@@ -10,6 +15,7 @@ export default function Toggler() {
   return (
     <>
       <h1 onClick={toggle}>{isHappy ? "😄" : "😢"}</h1>
+      <h1 onClick={handle}>{gender ? "👧" : "👦"}</h1>
     </>
   );
 }
