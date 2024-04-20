c0adbca7c964fe8d294e2680d7f84982d8a850dd
Yashaswini J <yashaswinij1999>
2024-04-20 10:43:18 +0530

    rendering form component

--- Attempt 1/Material UI/ratingComponent/src/Components/Forms.jsx
+++ Attempt 1/Material UI/ratingComponent/src/Components/Forms.jsx
@@ -1,15 +1,24 @@
 import TextField from "@mui/material/TextField";
+import { useState } from "react";
 
 export default function Forms() {
+  const [value, setValue] = useState("");
+
+  const handleChange = function (e) {
+    setValue(e.target.value);
+  };
+
   return (
     <>
+      <h1>Text : {value}</h1>
       <TextField
         id="standard-basic"
         label="Standard"
         variant="standard"
         placeholder="name"
         autoFocus
-        
+        value={value}
+        onChange={handleChange}
       />
     </>
   );
