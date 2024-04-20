d68ca082bfa6f32b83b7c0e3fecc53e2b096cba7
Yashaswini J <yashaswinij1999>
2024-04-20 10:39:04 +0530

    created new form component

--- Attempt 1/Material UI/ratingComponent/src/App.jsx
+++ Attempt 1/Material UI/ratingComponent/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Review from "./Components/Review";
+import Forms from "./Components/Forms";
 
 function App() {
   return (
     <>
-      <Review />
+      {/* <Review /> */}
+      <Forms />
     </>
   );
 }
---
+++ Attempt 1/Material UI/ratingComponent/src/Components/Forms.jsx
@@ -0,0 +1,16 @@
+import TextField from "@mui/material/TextField";
+
+export default function Forms() {
+  return (
+    <>
+      <TextField
+        id="standard-basic"
+        label="Standard"
+        variant="standard"
+        placeholder="name"
+        autoFocus
+        
+      />
+    </>
+  );
+}
