076d2a8e660cb93f239e97ae52de01dec2c57fd1
Yashaswini J <yashaswinij1999>
2024-04-27 13:16:48 +0530

    implement state and action as object

---
+++ Attempt 1/useReducer/CounterOne/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import CounterOne from "./Components/CounterOne";
+
+function App() {
+  return (
+    <>
+      <CounterOne />
+    </>
+  );
+}
+
+export default App;
---
+++ Attempt 1/useReducer/CounterOne/src/Components/CounterOne.jsx
@@ -0,0 +1,31 @@
+import { useReducer } from "react";
+
+const initialState = {
+  firstCounter: 0,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { firstCounter: state.firstCounter + 1 };
+    case "decrement":
+      return { firstCounter: state.firstCounter - 1 };
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
+
+export default function CounterOne() {
+  const [count, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <h1>Count : {count.firstCounter}</h1>
+      <button onClick={() => dispatch({ type: "increment" })}>increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>decrement</button>
+      <button onClick={() => dispatch({ type: "reset" })}>reset</button>
+    </>
+  );
+}
