0ec22f37152d6e881b6b65a3eb04d670d9de4bcf
Yashaswini J <yashaswinij1999>
2024-07-25 11:13:49 +0530

    displayed a pokemon responding to random number

--- Attempt 3/Die/src/Components/RandomPokemon.jsx
+++ Attempt 3/Die/src/Components/RandomPokemon.jsx
@@ -1,9 +1,12 @@
 function RandomPokemon() {
   let rand = Math.floor(Math.random() * 150) + 1;
-
+  let url = `    https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${rand}.png`;
   return (
     <>
-      <h1>Random Pokemon Number : {rand}</h1>
+      <div style={{ border: "1px solid", width: "450px", height: "300px" }}>
+        <h1 style={{ color: "blue" }}> Pokemon : #{rand}</h1>
+        <img src={url} style={{ width: "150px" }} />
+      </div>
     </>
   );
 }
