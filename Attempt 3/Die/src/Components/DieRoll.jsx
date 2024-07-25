964ff84d9050153396f3c8a6b2faefd15e8ac282
Yashaswini J <yashaswinij1999>
2024-07-25 10:54:19 +0530

    rendered two more die components

---
+++ Attempt 3/Die/src/Components/DieRoll.jsx
@@ -0,0 +1,14 @@
+import Die from "./Die";
+
+function DieRoll() {
+  return (
+    <>
+      <h1>Die Roll</h1>
+      <Die />
+      <Die />
+      <Die />
+    </>
+  );
+}
+
+export default DieRoll;
